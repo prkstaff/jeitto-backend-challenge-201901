ffd2e560a9c50efe6e749ce037013ec5261528f4
Renato Prado <prkstaff@gmail.com>
2019-05-12 17:25:10 -0300

    using test_client

--- tests/test_crud.py
+++ tests/test_crud.py
@@ -1,13 +1,16 @@
-from unittest import TestCase
-import requests
+import unittest
+from app import app
+import json
 
 
-class TestCrud(TestCase):
+class TestCrud(unittest.TestCase):
     def setUp(self):
+        app.config['TESTING'] = True
+        self.client = app.test_client()
         self.hostname = "http://localhost:5000"
 
     def test_get_companies_portfolio(self):
-        response = requests.get(self.hostname + "/CompanyProducts")
+        response = self.client.get("/CompanyProducts")
         headers = response.headers
 
         # assert Json Header
@@ -16,7 +19,7 @@ class TestCrud(TestCase):
         # assert status code
         self.assertEqual(response.status_code, 200)
 
-        portfolios = response.json()
+        portfolios = json.loads(response.data)
         for portfolio in portfolios:
             self.assertTrue(all(['company_id' in portfolio, 'products' in portfolio]))
             for product in portfolio['products']:
