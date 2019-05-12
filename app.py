53c1c65a05050b7a48534add50379fe0f8127b17
Renato Prado <prkstaff@gmail.com>
2019-05-11 21:19:47 -0300

    Basic test added, just a "mocked " data

--- app.py
+++ app.py
@@ -1,10 +1,26 @@
-from flask import Flask
+from flask import Flask, jsonify
 app = Flask(__name__)
 
 
-@app.route('/')
+@app.route('/CompanyProducts', methods=["GET"])
 def HelloWorld():
-    return 'Hello World :)'
+    companies_portfolio = [
+       {
+           "company_id": "claro_11",
+           "products":[
+               {"id": "claro_10", "value": 10.0},
+               {"id": "claro_20", "value": 20.0}
+           ]
+       },
+       {
+           "company_id": "tim_11",
+           "products":[
+               {"id": "tim_10", "value": 10.0},
+               {"id": "tim_20", "value": 20.0}
+           ]
+       }
+    ]
+    return jsonify(companies_portfolio)
 
 
 app.run(host='0.0.0.0')
--- tests/test_crud.py
+++ tests/test_crud.py
@@ -1,12 +1,23 @@
 from unittest import TestCase
 import requests
 
+
 class TestCrud(TestCase):
     def setUp(self):
         self.hostname = "http://localhost:5000"
 
-    def test_insert_company_portfolio(self):
-        req = requests.get(self.hostname + "/")
-        import pdb
-        pdb.set_trace()
+    def test_get_companies_portfolio(self):
+        response = requests.get(self.hostname + "/CompanyProducts")
+        headers = response.headers
 
+        # assert Json Header
+        self.assertEqual('application/json', headers['Content-Type'])
+
+        # assert status code
+        self.assertEqual(response.status_code, 200)
+
+        portfolios = response.json()
+        for portfolio in portfolios:
+            self.assertTrue(all(['company_id' in portfolio, 'products' in portfolio]))
+            for product in portfolio['products']:
+                self.assertTrue(all(['id' in product, 'value' in product]))
